28134d17b68c47f47dbc3c7fc31dd5ff6f217832
arthurbernhard715-cloud <arthurbernhard715@gmail.com>
2026-08-11 21:56:34 -0300

    Update extract-dados.js

--- extract-dados.js
+++ extract-dados.js
@@ -61,8 +61,14 @@ async function salvarDados(dados) {
 }
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method not allowed' });
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
     }
 
     try {
